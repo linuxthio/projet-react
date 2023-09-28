fe22c5812ae5ef8c2bf6944e2db9d439011dcc52
Djibril THIONGANE <linuxthio>
2023-09-28 03:36:01 +0000

    deploiement de projet-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <Navbar expand="lg" bg="light" >
+            <h1>Mon Application React</h1>
+          </Navbar>
+          <Card>
+            <Card.Title>Card 1</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Card 2</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Card 3</Card.Title>
+          </Card>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
